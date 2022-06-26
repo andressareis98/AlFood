951f793fc8e9e17fa7e0dd67e520eaca4fa7b9d4
Andressa Reis Araujo <andressareis98@outlook.com>
2022-06-26 15:57:54 -0300

    feat: função de adicionar novos restaurantes

---
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -0,0 +1,30 @@
+import { Button, TextField } from "@mui/material"
+import axios from "axios"
+import { useState } from "react"
+import IRestaurante from "../../../interfaces/IRestaurante"
+
+const FormularioRestaurante = () =>{
+
+    const [nomeRestaurante, setNomeRestaurante] = useState('')
+
+    const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
+        evento.preventDefault()
+
+        console.log('Preciso enviar dados para api: ')
+        console.log(nomeRestaurante)
+        axios.post('http://localhost:8000/api/v2/restaurantes/', {
+            nome: nomeRestaurante
+        })
+        .then(() => {
+           alert("Restaurante cadastrado com sucesso") 
+        })
+    }
+    return(
+        <form onSubmit={aoSubmeterForm}>
+            <TextField value={nomeRestaurante} onChange={evento => setNomeRestaurante(evento.target.value)} id="standard-basic" label="Standard" variant="standard" />
+            <Button type="submit" variant="outlined">Outlined</Button>
+        </form>
+    )
+}
+
+export default FormularioRestaurante
